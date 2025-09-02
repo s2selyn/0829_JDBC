937ec0bf3fe44871266e7f89e4c96697c11a5a32
unknown <s2selyn@gmail.com>
2025-09-02 21:23:40 +0900

    reviewed at 250902 to be continued

--- 01_JDBC_Statement/src/com/kh/statement/model/vo/Member.java
+++ 01_JDBC_Statement/src/com/kh/statement/model/vo/Member.java
@@ -4,32 +4,49 @@ import java.sql.Date;
 import java.util.Objects;
 
 /*
- * VO(Value Object) => 값 객체
- * => 테이블의 한 행에 대한 데이터를 기록할 저장용 객체
+ * VO(Value Object) => 값 객체(값을 담기 위한 용도, 무슨 값?)
+ * => 테이블의 한 행에 대한 데이터를 기록할 저장용 객체(일단 지금 기본적ㄱ으로)
  * 
  * VO가 가져야할 특성 == 불변성
  * 이친구는 지금 setter가 있음, 중간에 필드값이 언제든지 바뀔 수 있으므로 세터를 가지면 안됨
  * 무조건 생성자를 이용해서 값을 담아야함, 값에 변화가 일어나면 안되니까
  * 
- * VO클래스의 필드 구성 자체를 MEMBER테이블의 컬럼들과 유사하게 구성
+ * 지금부터 만들 프로그램은 회원 관리 프로그램
+ * 회원의 정보들을 이런것들을 저장해놔야지 하고 테이블 구성해둠
+ * 새로운 회원이 들어온다는 것은 한 명의 회원의 정보는 한 행으로 들어올 것
+ * 추가할거면 값이 여섯개 필요함, 이걸 자바에서 들고가야함, 실질적으로 업무 해결에 필요한 데이터들, 이것들을 담을 무언가가 필요함 --> VO가 그 역할을 하겠다
+ * Member는 테이블의 한 행의 데이터를 담을 수 있도록 구성할 것 --> 클래스가 가지는 속성을? 필드(객체의 속성을 담는 친구) --> 필드 구성을 테이블의 모양으로 구성(테이블의 모양, 테이블이 가지는 컬럼)
+ * 클래스가 가질 수 있는 건 세개밖에 없음(필드 생성자 메소드)
+ * 
+ * VO클래스의 필드 구성 자체를 MEMBER테이블의 컬럼들과 유사하게 구성(똑같이는 할 수 없음, 오라클의 데이터 타입이랑 자바에서의 데이터 타입이 다름)
+ * 최대한 유사하게 구성을 해서 테이블에 담을 데이터 또는 테이블에서 조회하는 데이터를 이 친구를 객체로 올려가지고 거기에 담을 수 있도록 만들것
+ * USERNO와 ENROLLDATE 컬럼은 사용자에게 값을 입력받지 않고 ENROLLDATE는 SYSDATE 넣을거고 USERNO는 시퀀스로 만들것 --> 생성자를 만들 때 이 두개만 빠진 생성자를 추가로 만들 것
  * SEQUENCE 및 DEFAULT값을 조건으로 사용하는 경우
  * 해당 필드를 제외한 나머지 필드값을 초기화 할 수 있는 생성자를 추가로 구성해둘 것
  * 
  */
 public class Member {
 	
-	private int userNo;
-	private String userId;
-	private String userPwd;
-	private String userName;
-	private String email;
-	private Date enrollDate; // sql.Date로 import
-	// DB의 테이블을 참고하여 컬럼과 유사하게 구성
+	// VO클래스 만들 때 항상 만들면서 추상화를 진행했는데 이제는 항상 테이블이 먼저 만들어지고 그다음 클래스가 만들어지는것, 테이블이 있어야 무멀 넣을지가 정해진거니까
+	// 앞으로는 만들 때 크게 고민할 필요가 없음, 테이블 모양대로 만들면 됨
+	private int userNo; // USERNO NUMBER PRIMARY KEY, DB의 숫자값으로 들어갈건데 자바에서는 int, 들어가는 숫자가 22억이 넘을리 없을것같고
+	private String userId; // USERID VARCHAR2(15) UNIQUE NOT NULL, 오라클에서 최대 15자의 문자열 형태의 데이터는 자바에서 스트링
+	private String userPwd; // USERPWD VARCHAR2(20) NOT NULL
+	private String userName; // USERNAME VARCHAR2(15) NOT NULL
+	private String email; // EMAIL VARCHAR2(30)
+	private Date enrollDate; // ENROLLDATE DEFAULT SYSDATE NOT NULL <-- java.sql.Date로 import
+	// 오라클의 날짜형식과 자바의 날짜 형식은 완전히 다름, 어떻게 할 지 정해야함
+	// 자바에서 SQL 타입의 데이터를 다루기 위한 친구는 따로 API로 구현되어 있음, 처음이니까 정석대로 Date 타입으로
+	// 필드부를 DB의 테이블을 참고하여 컬럼과 유사하게 구성
 	
+	// 필드부 다음에 생성자부
+	// 기본 생성자
 	public Member() {
 		super();
 	}
 	
+	// USERNO는 시퀀스를 가지고 만들어줄거고 ENROLLDATE는 디폴트 값이 있음
+	// 추가로 생성자를 만들 때 기본값으로 넣는 필드 두개 뺀 나머지만 초기화해주는 생성자를 하나 더 넣어줌
 	public Member(String userId, String userPwd, String userName, String email) {
 		super();
 		this.userId = userId;
@@ -39,7 +56,7 @@ public class Member {
 	}
 	// 기본값이 존재하는 컬럼을 제외하고 나머지 필드값만 초기화 해주는 생성자 추가
 	
-
+	// 필드 전부 있는 생성자
 	public Member(int userNo, String userId, String userPwd, String userName, String email, Date enrollDate) {
 		super();
 		this.userNo = userNo;
@@ -50,6 +67,8 @@ public class Member {
 		this.enrollDate = enrollDate;
 	}
 
+	// 메소드부는 똑같음
+	// 게터세터
 	public int getUserNo() {
 		return userNo;
 	}
@@ -98,17 +117,22 @@ public class Member {
 		this.enrollDate = enrollDate;
 	}
 
+	// toString
 	@Override
 	public String toString() {
 		return "Member [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
 				+ ", email=" + email + ", enrollDate=" + enrollDate + "]";
 	}
 
+	// hashCode
+	// 만약에 필드값이 같다면 테이블에 데이터가 똑같다는 것
+	// PK가 있기 때문에 PK가 똑같으면 같은 행이라는 뜻, 그러면 같은 데이터라고 인식을 시켜야함, 그러므로 VO는 equals, hashCode가 오버라이딩 되어있어야함
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, enrollDate, userId, userName, userNo, userPwd);
 	}
 
+	// equals
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
--- 01_JDBC_Statement/src/com/kh/statement/view/MemberView.java
+++ 01_JDBC_Statement/src/com/kh/statement/view/MemberView.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import com.kh.statement.controller.MemberController;
 import com.kh.statement.model.vo.Member;
 
-//Alt + Shift + j
+// Alt + Shift + j --> 라이브러리 주석
 /**
  * 
  * MemberView 클래스는 JDBC 실습을 위해 생성하였으며,
@@ -19,18 +19,46 @@ import com.kh.statement.model.vo.Member;
  */
 public class MemberView {
 	
-	private Scanner sc = new Scanner(System.in);
-	private MemberController mc = new MemberController();
+	// 사용자한테 뭔가 값을 입력받고 나중에 출력을 해주는 용도로 만들어주는 클래스
+	// 입력을 받은걸 가지고 DB까지 가서 인서트하거나 업데이트하거나 딜리트하거나 또는 셀렉을 할때에 WHERE 조건절에 조건으로 사용하거나 이렇게 쓸 예정
+	// 나중에 출력을 할 때는 출력문을 통해서 DB에 멤버테이블에 있는 데이터를 가지고 와서 화면상에 띄워줄것
 	
+	// 입력을받아야되는데 뷰클래스 안에 있는 메소드들은 전부 스캐너가 필요할것 --> 메소드 레벨로 선언하지 않고 어차피 여기서 다 써야되니까 필드로 구성
+	private Scanner sc = new Scanner(System.in); // 뷰 클래스가 객체로 올라감과 동시에 같이 올라가게끔 구성
+	
+	// 뷰는 입력 받으면 끝, 출력해주면 끝이고 실질적으로 DB랑 왔다갔다는 DAO가 함(DB에 작업하고 DB에 인서트하고 뭐시기하고)
+	// 뷰가 DAO까지 입력받은걸 넘겨줘야함, 근데 다이렉트로 가요? 아니면 누가 중간에 껴요? 중간에 컨트롤러를 거쳐서 넘어갈거임
+	// 컨트롤러도 뷰 안에 가진 멤버 함수 안에서는 다 사용을 해야함, 컨트롤러도 필드로 구성
+	private MemberController mc = new MemberController();
+	// 컨트롤러가 DAO를 호출하게 만들것이므로 컨트롤러의 멤버를 사용하고싶은것임 --> 굳이 상속을 안하고 컨트롤러를 필드로 두고 컨트롤러의 멤버들을 호출해서 사용 --> 합성, Composition
+	
+	// 이 안에 프로그램 실행하면 제일 먼저 보여줄 메인 화면 만들거
+	// 메소드를 만들면 주석을 또 예쁘게 작성
+	// 클래스 들어오면 한눈에 보기 좋고 다른 클래스에서 객체를 생성해서 호출할 때 설명이 나옴 사용하는 쪽에서 이 클래스에 들어와보지 않아도 볼 수 있음
+	// 메소드도 설명 보임 요요도를 확인할 수 있음
+	// 주석을 예쁘게 잘 달아두면 나중에 다시볼때도, 다른 사람이 볼때도 굉장히 좋겠다
+	// 지금은 배우는 과정이니까 이렇게까지 적을 필요는 없음, 나중에 프로젝트 할 때라든지 아니면 회사 가서라든지 가서는 항상 주석을 이렇게 예쁘게
 	/**
 	 * 프로그램 구동 시 사용자에게 메인화면을 출력해주는 메소드입니다.
 	 * 
 	 */
 	public void mainMenu() {
 		
+		// 프로그램 시작하면 메인 메소드에서 제일 처음에 객체로 올려서 호출할 메소드
+		// 프로그램 실행하면 뜨는 메인화면이 떴다고 가정했을 때 출력해줄 내용
+		// 기본적으로 계속해서 프로그램을 종료하기 전까지는 반복해서 화면을 보여줄거니까 while문 사용
 		while(true) {
 			
+			// 안에는 출력해 줄 수 있는 방법이 출력문 밖에 없음, 출력문으로 출력해줄것
+			// 뷰는 지금 우리한테 별로 그렇게 중요한 요소가 아님, 뷰에서 뭘 해야한다 이건 중요
+			// 뷰에 적는 코드들은 별로 중요하지 않음, 왜? 다음주에 배울 HTML CSS JavaScript로 대체가 되는 친구니까
+			// 지금은 우리가 텍스트로 이렇게 보여주고 있지만 우리 웹개발자니까 웹브라우저에서 화면이 나와야함, 지금 이걸 화면으로 쓸 순 없음, 다음주에 배울 친구들로 화면은 대체될것임
+			// 뷰는 대체가 되어야함, 그래서 여기에 적는 코드들은 별로 중요하지 않음
+			// 그렇지만 여기서 해야 할 일은 변하지 않음 --> 사용자에게 무언가를 보여주고 사용자한테 값을 입력받아서 그것을 컨트롤러한테 보내줘야됨
 			System.out.println(" ---- 회원 관리 프로그램 ---- ");
+			
+			// 회원 관리 프로그램에서 총 여섯가지 구현
+			// Plant에서 하던거랑 똑같음, 항상 우리가 하는 일은 똑같음
 			System.out.println("1. 회원 추가");
 			System.out.println("2. 회원 전체 조회");
 			System.out.println("3. 회원 아이디로 조회");
@@ -39,24 +67,29 @@ public class MemberView {
 			System.out.println("6. 회원 탈퇴");
 			System.out.println("9. 프로그램 종료");
 			System.out.print("메뉴를 선택해주세요 > ");
-			int menuNo = sc.nextInt();
+			int menuNo = sc.nextInt(); // 스캐너가 가지고 있는 메소드를 이용해서 값을 입력받음
 			sc.nextLine();
 			
+			// 사용자가 정수값을 뭘 입력했냐에 따라서 다른 기능을 수행할 수 있도록 switch문
+			// 예외처리는 생략하고 JDBC에 집중
 			switch(menuNo) {
-			case 1 : save(); break;
+			case 1 : save(); break; // 회원 추가할래요 했으면 뷰에서는? 웹사이트에서 내가 회원 가입해야지 하고 누루면 뭐가 나와야함? 아이디 비밀번호 입력하세요 정보를 입력할 수 있는 화면이 나와야함
 			case 2 : findAll(); break;
 			case 3 : break;
 			case 4 : break;
 			case 5 : update(); break;
 			case 6 : delete(); break;
-			case 9 : System.out.println("프로그램을 종료합니다."); return;
-			default : System.out.println("잘못된 메뉴 선택입니다.");
+			case 9 : System.out.println("프로그램을 종료합니다."); return; // 종료를 입력했을 때는 return으로 메인메소드로 보냄
+			default : System.out.println("잘못된 메뉴 선택입니다."); // 이상한 거 누른거니까 그냥 메시지 정도만 출력
 			}
 			
 		}
 		
 	}
 	
+	// 항상 기능을 만들 때는 기존에 있는 걸 생각
+	// 지금 필요한게 테이블에 넣을 여섯개의 값, INSERT 할 때 한 행으로 넣어야하니까 총 여섯개의 값이 필요함
+	// USERNO는 시퀀스로 넣기로 함, ENROLLDATE는 가입시점에 들어가니까 SYSDATE넣으면됨, 사용자한테 네개를 입력받아야함 --> 아이디 PWD 네임 이메일
 	/**
 	 * MEMBER 테이블에 INSERT할 값을 사용자가 입력받는 화면을 출력해주는 메소드
 	 * 
@@ -65,8 +98,9 @@ public class MemberView {
 	 */
 	private void save() {
 		
-		System.out.println("--- 회원 추가 --- ");
+		System.out.println("--- 회원 추가 ---");
 		
+		// 입력받고 이런거 우리 너무 익숙
 		System.out.print("아이디를 입력해주세요 > ");
 		String userId = sc.nextLine();
 		
@@ -80,7 +114,11 @@ public class MemberView {
 		String email = sc.nextLine();
 		
 		// 일단 View에서 할 일은 끝남 -> 컨트롤러로 요청 처리
+		// DB가 붙었으니까 회원 추가해주세요 하면 INSERT 하는게 목적임, 최종 목적은 DB에 INSERT하는거
+		// INSERT에 필요한 값 다 받았으면 끝, 네개의 값을 컨트롤러로 전달, 메소드 호출하면서 호출 시 인자값으로 보냄
+		// 앞에서 하던거에 JDBC 붙인거
 		int result = mc.save(userId, userPwd, userName, email);
+		// 컨트롤러 ㄱㄱ
 		
 		// Controller에서 받은 반환값으로 결과 출력
 		if(result > 0) {
